2a8dfb28e17bad4b333977e63515515804214509
TUX <tuxwhitetiger@gmail.com>
2022-11-27 22:12:48 +0000

    add startup script

---
+++ CybertoothTiger/Scripts/Startup-Script.sh
@@ -0,0 +1,2 @@
+﻿git -C /home/tux/GIT/CybertoothTiger pull https://github.com/tuxwhitetiger/CybertoothTiger.git
+dotnet run --project /home/tux/GIT/CybertoothTiger/CybertoothTiger/CybertoothTiger.csproj
