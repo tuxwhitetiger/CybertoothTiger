ca4a8d79d781b8f4893afc47ff3ea5824176954e
TUX <tuxwhitetiger@gmail.com>
2022-11-27 22:23:43 +0000

    .

--- CybertoothTiger/Scripts/Startup-Script.sh
+++ CybertoothTiger/Scripts/Startup-Script.sh
@@ -1,4 +1,3 @@
 ﻿#!/bin/bash
 git -C /home/tux/GIT/CybertoothTiger pull https://github.com/tuxwhitetiger/CybertoothTiger.git
 dotnet run --project /home/tux/GIT/CybertoothTiger/CybertoothTiger/CybertoothTiger.csproj
-
