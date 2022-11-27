c02b07bbce92d9c0c62b74016b2ea205615f873b
TUX <tuxwhitetiger@gmail.com>
2022-11-27 22:18:47 +0000

    update

--- CybertoothTiger/Scripts/Startup-Script.sh
+++ CybertoothTiger/Scripts/Startup-Script.sh
@@ -1,2 +1,4 @@
-﻿git -C /home/tux/GIT/CybertoothTiger pull https://github.com/tuxwhitetiger/CybertoothTiger.git
+﻿#!/bin/bash
+git -C /home/tux/GIT/CybertoothTiger pull https://github.com/tuxwhitetiger/CybertoothTiger.git
 dotnet run --project /home/tux/GIT/CybertoothTiger/CybertoothTiger/CybertoothTiger.csproj
+
